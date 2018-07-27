73cfb1f5f5e5df6f195507a81bb615e539b2c2a6
Juliana Viana <41312878+JulianaGViana@users.noreply.github.com>
2018-07-27 19:06:00 -0300

    Implementação do código das funções

--- sistema.js
+++ sistema.js
@@ -21,48 +21,126 @@ function SistemaCadastro() {
 
     function adicionarParticipante(nome, sobrenome, email, idade, sexo) {
         //implemente o código necessário
-        var p = new Participante();
-        p.nome = nome;
-        p.sobrenome = sobrenome;
-        p.email = email;
-        p.idade = idade;
-        p.sexo = sexo;
-
-        participantes.push(p);
+		var check = -1;
+		for(var i = 0; i < participantes.length; i++){
+			if(participantes[i].email === email){
+				check = 1;
+			}
+		}
+		if(check === -1){
+			var p = new Participante();
+			p.nome = nome;
+			p.sobrenome = sobrenome;
+			p.email = email;
+			p.idade = idade;
+			p.sexo = sexo;
+			
+			participantes.push(p);
+			}else{
+				throw new Participante();
+			}							
     }
 
     function removerParticipante(email) {
-        //implemente o código necessário       
+        //implemente o código necessário
+		for(var i = 0; i < participantes.length; i++){
+			if(participantes[i].email===email){
+				participantes.splice(i,1);
+			}
+		}			
     }
     function buscarParticipantesPorNome(nome){
         //implemente o código necessário
+		var array = [];
+		for(var i = 0; i < participantes.length; i++){
+			if(participantes[i].nome === nome){
+				array.push(participantes[i]);
+			}
+		}
+		return array;
     }    
     function buscarParticipantesPorSexo(sexo){
         //implemente o código necessário
+		var array = [];
+        for(var i = 0; i < participantes.length; i++){
+            if(participantes[i].sexo === sexo){
+                array.push(participantes[i]);
+            }
+        }
+		return array;
     }
+	
     function buscarParticipantesAprovados(){
         //implemente o código necessário
+		var aprov = [];
+		for(var i = 0; i < participantes.length; i++){
+			if(participantes[i].aprovado === true){
+				aprov[i] = participantes[i].aprovado;
+			}
+		}
+		return aprov;
+				
     }
     function buscarParticipantesReprovados(){
         //implemente o código necessário
+		var reprov = [];
+		for(var i = 0; i < participantes.length; i++){
+			if(participantes[i].aprovado === false){
+				reprov[i] = participantes[i].aprovado;
+			}
+		}
+		return reprov;
     }
     function obterParticipante(email){
         //implemente o código necessário
+		for(var i = 0; i < participantes.length; i++){
+			if(participantes[i].email===email){
+				return participantes[i];
+			}
+		}
     }
     function adicionarNotaAoParticipante(email, nota){
         //implemente o código necessário
+		for(var i = 0; i < participantes.length; i++){
+            if(participantes[i].email === email){
+                participantes[i].nota = nota;
+                if(participantes[i].nota >= 70){
+                    participantes[i].aprovado = true;
+                }
+                else{
+                    participantes[i].aprovado = false;
+                }
+            }
+        }
     }
     function obterMediaDasNotasDosParticipantes(){
         //implemente o código necessário
+		var soma = 0;
+        for(var i = 0; i < participantes.length; i++){
+            soma += participantes[i].nota;
+        }
+        return soma/participantes.length;
     }
     function obterTotalDeParticipantes(){
         return participantes.length;
     }
     function verificarSeParticipanteEstaAprovado(email){
         //implemente o código necessário
+		for(var i = 0; i < participantes.length; i++){
+			if(participantes[i].email === email){
+				return participantes[i].aprovado;
+			}
+		}
     }
     function obterQuantidadeDeParticipantesPorSexo(sexo){
         //implemente o código necessário
+		var resultado = 0;
+		for(var i = 0; i < participantes.length; i++){
+			if(participantes[i].sexo === sexo){
+				resultado++;
+			}
+		}
+		return resultado;
     }
 
     return {
