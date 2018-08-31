ea2360245fc68022e0168a555b3aa1360dcf23cb
JulianaGViana <juliana.gviana@hotmail.com>
2018-08-31 00:24:53 -0300

    Alteração de funções e ajustes conforme feedback.

--- DOM.js
+++ DOM.js
@@ -1,33 +1,41 @@
 var sistema = new SistemaCadastro();
 var valorRadio = 0;
 var edicao = false;
+var idParticipante = 0;
 
 function inicioProcesso(){
 	modificarTabela();
 }
 
-function alterarParticipante(email){
+function alterarParticipante(id){
+	idParticipante = id;
 	checarRadio();
-	var participante = sistema.obterParticipante(email);
-	document.getElementById("nomeParticipante").value = participante.nome,
-	document.getElementById("sobrenomeParticipante").value = participante.sobrenome;
-	document.getElementById("emailParticipante").value = participante.email;
-	document.getElementById("emailParticipante").disabled = true;
-	document.getElementById("idadeParticipante").value = participante.idade;
-	document.getElementById("notaParticipante").value = participante.nota;
-	valorRadio;
-	edicao = true;
+	sistema.obterParticipante(id)
+	.then(function(participante){
+		document.getElementById("nomeParticipante").value = participante.nome,
+		document.getElementById("sobrenomeParticipante").value = participante.sobrenome;
+		document.getElementById("emailParticipante").value = participante.email;
+		document.getElementById("emailParticipante").disabled = true;
+		document.getElementById("idadeParticipante").value = participante.idade;
+		document.getElementById("notaParticipante").value = participante.nota;
+		valorRadio;
+		edicao = true;
+	});
 }
 
-function excluirParticipante(email){
-	sistema.removerParticipante(email);
-	alert("Participante excluído com sucesso!");
-	window.location.reload(true);
+function excluirParticipante(id){
+	sistema.removerParticipante(id)
+	.then(function(){
+		window.location.reload(true);
+	})
 }
 
 function modificarTabela(){
-    sistema.obterParticipantes().forEach(function(elemento, index){
-        document.querySelector('#informacoes').innerHTML += "<tr><td>"+(index+1)+"</td><td>"+elemento.nome+" "+elemento.sobrenome+"</td><td>"+elemento.idade+"</td><td>"+elemento.sexo+"</td><td>"+elemento.nota+"</td><td>"+elemento.aprovado+"</td><td>"+'<a href="javascript:void(0)" onclick="alterarParticipante(\''+elemento.email+'\')">Editar Participante</a>'+' | '+'<a href="javascript:void(0)" onclick="excluirParticipante(\''+elemento.email+'\')">Excluir Participante</a>'+"</td></tr>";
+    sistema.obterParticipantes() 
+    .then(function(res){
+    	res.forEach(function(elemento, index){
+        	document.querySelector('#informacoes').innerHTML += "<tr><td>"+(index+1)+"</td><td>"+elemento.nome+" "+elemento.sobrenome+"</td><td>"+elemento.idade+"</td><td>"+elemento.sexo+"</td><td>"+elemento.nota+"</td><td>"+elemento.aprovado+"</td><td>"+'<a href="javascript:void(0)" onclick="alterarParticipante(\''+elemento.id+'\')">Editar Participante</a>'+' | '+'<a href="javascript:void(0)" onclick="excluirParticipante(\''+elemento.id+'\')">Excluir Participante</a>'+"</td></tr>";
+    	});
     });
 }
 
@@ -45,41 +53,56 @@ function desabilitarEdicao(){
 	document.getElementById("emailParticipante").disabled = false;
 }
 
+function criticaCampo(){
+	if(document.getElementById("nomeParticipante").value === "" || document.getElementById("sobrenomeParticipante").value === "" || document.getElementById("emailParticipante").value === "" || document.getElementById("idadeParticipante").value === "" || document.getElementById("notaParticipante").value === "")
+	{
+		return true;
+	}
+	else
+		return false;
+}
+
 inicioProcesso();
 var salvar = document.getElementById("salvarCadastro");
 salvar.addEventListener('click', function(e){
-	if(edicao){
-		checarRadio();
-		sistema.editarParticipante(
-			document.getElementById("nomeParticipante").value,
-			document.getElementById("sobrenomeParticipante").value,
-			document.getElementById("emailParticipante").value,
-			document.getElementById("idadeParticipante").value,
-			valorRadio,
-			document.getElementById("notaParticipante").value
-			);
-			window.location.reload(true);
-	}
-	else
-	{
-		try{
+	criticaCampo();
+	if(criticaCampo())
+		alert("Os campos não podem permanecer vazios!");
+	else{
+		if(edicao){
+			checarRadio();
+			sistema.editarParticipante(
+				idParticipante,
+				document.getElementById("nomeParticipante").value,
+				document.getElementById("sobrenomeParticipante").value,
+				document.getElementById("emailParticipante").value,
+				document.getElementById("idadeParticipante").value,
+				valorRadio,
+				document.getElementById("notaParticipante").value
+			)
+			.then(function(){
+				window.location.reload(true);
+			});
+		}
+		else
+		{
 			checarRadio();
 			sistema.adicionarParticipante(
 				document.getElementById("nomeParticipante").value,
 				document.getElementById("sobrenomeParticipante").value,
 				document.getElementById("emailParticipante").value,
 				document.getElementById("idadeParticipante").value,
-				valorRadio
-			);
-
-			sistema.adicionarNotaAoParticipante(
-				document.getElementById("emailParticipante").value,
+				valorRadio,
 				document.getElementById("notaParticipante").value
-				);
-			window.location.reload(true);
-		}catch(Error){
-			window.alert(Error.message);
-		}
+			)
+			.then(function(){
+				window.location.reload(true);
+			})
+			.catch(function(error){
+				alert(error);
+				window.location.reload(true);
+			});
 	}
 	desabilitarEdicao();
+	}
 });
